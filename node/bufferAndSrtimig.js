43e759d82d4840d77cab9f331b16d051b4009ca9
atik007 <atikhasan1971arik@gmail.com>
2024-12-20 01:40:45 +0600

    Writestrem

--- node/bufferAndSrtimig.js
+++ node/bufferAndSrtimig.js
@@ -1,46 +1,13 @@
 const http = require('http');
-const { buffer } = require('stream/consumers');
-//create new server
+const fs = require('fs');
+
+
 const server = http.createServer((req, res)=>{
-
-    if(req.url === '/'){
-
     
-                res.write('<html><head><title>From</title></head></html>');
-                res.write('<body><form method="post" action="/process"> <input name="message"/></form></body>');
-                res.end();
-
-
-    } else if(req.url==='/process' && req.method==="POST"){
-
-        const body = [];
-        
-        req.on('data',(chunk)=>{
-            body.push(chunk);
-        })
-
-         
-        req.on('end',()=>{
-            console.log("stream finished");
-            const parseBody = Buffer.concat(body).toString();
-            console.log(parseBody);
-        })
-        res.write("thank you for submitted ");
-        res.end();
-
-       
-    }
-    else {
-        res.write('Not found');
-        res.end();
-    }
+    const myReadStream = fs.createReadStream(__dirname + '/myfile.txt', 'utf-8')
+    myReadStream.pipe(res);
+     
 });
 server.listen(3000)
 console.log('listening on prot 3000');
  
-// const fs = require('fs');
-// const ourReadStream = fs.createReadStream(`${__dirname}/myfile.txt`,'utf8');
-// ourReadStream.on('data', (data)=>{
-//   console.log(data);
-// })
-// console.log("hello");
