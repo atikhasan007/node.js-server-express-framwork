3fc988bc650133baaa8f5a9da4d58fce3d5ad4bc
atik007 <atikhasan1971arik@gmail.com>
2024-12-22 00:12:54 +0600

    found path

--- src/index.js
+++ src/index.js
@@ -13,7 +13,9 @@
 
 
 // dependencies 
+const { copyFileSync } = require('fs');
 const http = require('http');
+const url = require('url');
 
 //app object  - module scaffolding
 const app = {}
@@ -34,6 +36,17 @@ app.createServer = () =>{
 
 //handle request response
 app.handleRequest = (req, res) => {
+    //request handling
+    //get the url and parse it 
+    const parsedUrl = url.parse(req.url, true);
+    const path = parsedUrl.pathname;
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+    console.log(trimmedPath);
+
+
+
+
+
     //response handle
     res.end('hello world programmer');
 }
@@ -42,3 +55,10 @@ app.handleRequest = (req, res) => {
 
 //start the server 
  app.createServer();
+
+
+
+
+
+
+
