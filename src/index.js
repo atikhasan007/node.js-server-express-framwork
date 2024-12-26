5010d27970d0bbe9adea94d4a4c1d229ed5630ea
atik007 <atikhasan1971arik@gmail.com>
2024-12-27 03:38:59 +0600

    file create path

--- src/index.js
+++ src/index.js
@@ -4,14 +4,25 @@
 3.Author      : md atik hasan 
 4.Date        : 19/12/24
 */
-
 // Dependencies
 const http = require('http');
 const { handleReqRes } = require('../helpers/handleReqRes'); // Ensure the path is correct
 const environment = require('../helpers/environment'); // Ensure the path is correct
-
+const data = require('../lib/data');
 // App object - Module scaffolding
 const app = {};
+
+
+//testing file system
+//@todo pore muche dibo
+
+data.create('test', 'newFile' , {
+    'name':'bangladesh',
+    'language':'bangla'
+}, (err)=>{
+    console.log(`error was`, err);
+})
+
 
 // Create server
 app.createServer = () => {
